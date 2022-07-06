5cef636b0ce1ff2bfb3e827b2d189e5c9076d60c
Madgere <madgerer@gmail.com>
2022-07-06 16:15:51 +0300

    category tab layout

--- src/app/newProduct/newProductTabs/NewProductTabs.tsx
+++ src/app/newProduct/newProductTabs/NewProductTabs.tsx
@@ -1,9 +1,10 @@
 import {Tab, Tabs} from "react-bootstrap";
+import CategoryTab from "./tabs/CategoryTab";
 
 export default function NewProductTabs() {
     return <Tabs defaultActiveKey="tablePart" id="newProductTabs" className="mb-3">
         <Tab eventKey="category" title="Категории">
-            Категории
+            <CategoryTab/>
         </Tab>
         <Tab eventKey="tablePart" title="Табличная часть">
             Табличная часть
---
+++ src/app/newProduct/newProductTabs/tabs/CategoryTab.tsx
@@ -0,0 +1,59 @@
+export default function CategoryTab() {
+    return <div className="tab-pane row">
+        <div className="item col-md-12">
+            <div>
+                <h2>Категории в каталоге</h2>
+                <button id="changeCat" type="button" className="btn btn-dark">
+                    <i className="fa  fa-plus" aria-hidden="true"></i>
+                </button>
+                <button id="changeCat" type="button" className="btn btn-dark">
+                    <i className="fa fa-pencil-square-o" aria-hidden="true"></i>
+                </button>
+                <button id="changeCat" type="button" className="btn btn-dark">
+                    <i className="fa fa fa-minus" aria-hidden="true"></i>
+                </button>
+                Селектор печатных каталогов
+                <div>Раскрывающийся селектор</div>
+            </div>
+            Вот тут какая-то нерабочая таблица
+        </div>
+        <div className="item col-md-12">
+            <div>
+                <h2>Категории на сайт</h2>
+                <button id="changeCat" type="button" className="btn btn-dark">
+                    <i className="fa  fa-plus" aria-hidden="true"></i>
+                </button>
+                <button id="changeCat" type="button" className="btn btn-dark">
+                    <i className="fa fa-pencil-square-o" aria-hidden="true"></i>
+                </button>
+                <button id="changeCat" type="button" className="btn btn-dark">
+                    Сделать главной
+                </button>
+                <button id="changeCat" type="button" className="btn btn-dark">
+                    <i className="fa fa fa-minus" aria-hidden="true"></i>
+                </button>
+                <div>Раскрывающийся селектор</div>
+            </div>
+            Вот тут какая-то нерабочая таблица
+        </div>
+        <div className="item col-md-12">
+            <div>
+                <h2>Категории на сайт</h2>
+                <button id="changeCat" type="button" className="btn btn-dark">
+                    <i className="fa  fa-plus" aria-hidden="true"></i>
+                </button>
+                <button id="changeCat" type="button" className="btn btn-dark">
+                    <i className="fa fa-pencil-square-o" aria-hidden="true"></i>
+                </button>
+                <button id="changeCat" type="button" className="btn btn-dark">
+                    Сделать главной
+                </button>
+                <button id="changeCat" type="button" className="btn btn-dark">
+                    <i className="fa fa fa-minus" aria-hidden="true"></i>
+                </button>
+                <div>Селектор scope of application</div>
+            </div>
+            Какая-то не рабочая таблица
+        </div>
+    </div>
+}
